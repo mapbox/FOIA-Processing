95b76713320a733a419b220beb96d77987cd0e9c
Andrea del Rio <adelrio@gmail.com>
2019-02-26 10:11:14 -0800

    add json to gitignore, rm old code from script

--- process_shp.sh
+++ process_shp.sh
@@ -31,36 +31,29 @@ fi
 
 
 echo "Unzipping data..."
-# mkdir unzipped
+mkdir unzipped
 
 cd unzipped
-# unzip "../${FILEPATH}"
+unzip "../${FILEPATH}"
 
 echo "Processing data files..."
 OUTFILE=$OUTPUT
 echo $OUTFILE
 
+# Process shapefile file to expected projection
 ogr2ogr -f "ESRI Shapefile" -t_srs "EPSG:4326" ../$OUTFILE *.shp
+
+# Generate output for open addresses review
 ogr2ogr -f "Geojson" -t_srs "EPSG:4326" ../$OUTFILE.geojson *.shp
-# ogr2ogr -f "Geojson" -t_srs "EPSG:4326" ../pumpkin.geojson *.shp
-# cat ../pumpkin.geojson | jq .
-# jq '.features[0:1][0].properties |keys' ../pumpkin.geojson
 jq '.features[0:1][0].properties |keys' ../$OUTFILE.geojson
-jq '.features[0]' ../$OUTFILE.geojson
-# FEATURES=`jq '.features[0]' ../$OUTFILE.geojson`
 
 for (( i = 0; i < 5; i++ )); do
-    #statements
     FEATURES="`jq .features[$i] ../$OUTFILE.geojson`"
-    echo $FEATURES
-    echo $FEATURES -o output.json
+    (jq .features[$i] ../$OUTFILE.geojson) >> ../$OUTFILE.json
 done
-echo $FEATURES
-
 cd ../$OUTFILE
-# ogrinfo -so *.shp
 
-
+# Rename files to expected output name
 for FILE in *
 do
     echo "converting file: $FILE..."
@@ -70,26 +63,15 @@ do
     zip $OUTPUT.zip ${OUTFILE}
 done
 
-SHP=$OUTPUT.shp
-echo $SHP
-
-# QUERY="SELECT * FROM ${SHP%.*} WHERE 1"
-# QUERY="SELECT * FROM apples.shp WHERE 1"
-# echo $QUERY
-
-# ogrinfo -q $SHP -sql "SELECT * FROM apples LIMIT 10"
-# ogr2ogr -f "Geojson" -t_srs "EPSG:4326" $SHP "SELECT * FROM ${SHP%.*} LIMIT 10" -dialect SQLITE
-
-
+# Cleanup
 echo "Finishing up"
 cd ../
 mv $OUTPUT/$OUTPUT.zip ./$OUTPUT.zip
 rm -rf $OUTPUT/
-# rm -rf unzipped/
+rm -rf unzipped/
 
 # TODO: rm
-rm $OUTPUT.zip
-rm $OUTPUT.geojson
-# rm pumpkin.geojson
+# rm $OUTPUT.zip
+# rm $OUTPUT.geojson
 
 exit 0
